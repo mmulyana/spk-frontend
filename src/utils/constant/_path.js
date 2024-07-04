e34c57dca0de6bbee9edb0e2ad151ca49e138de8
mmulyana <mulyan.t20@gmail.com>
2024-07-05 02:31:36 +0700

    chore: update path constant

--- src/utils/constant/_path.js
+++ src/utils/constant/_path.js
@@ -1,7 +1,13 @@
 export const PATH = {
+  LOGIN: '/login',
+  FORGOT: '/forgot',
+  REGISTER: '/register',
+  
+  DASHBOARD_ADMIN_ACCOUNT: '/dashboard/admin/account',
   DASHBOARD: '/dashboard',
   DASHBOARD_EMPLOYEE: '/dashboard/employee',
   DASHBOARD_CRITERIA: '/dashboard/criteria',
-  DASHBOARD_ADMIN: '/dashboard/admin',
-  DASHBOARD_ADMIN_ACCOUNT: '/dashboard/admin/account',
+  DASHBOARD_ADMIN_SETTINGS: '/dashboard/admin',
+  
+  NOT_FOUND: '*',
 }
