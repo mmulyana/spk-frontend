aab709d673659d448bbffcf9317d7a65cfbbff46
mmulyana <mulyan.t20@gmail.com>
2024-07-05 02:30:25 +0700

    feat: update routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,26 +1,43 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
 import LoaderScreen from '../components/loader'
+import { PATH } from '../utils/constant/_path'
 const Login = lazy(() => import('./auth/login'))
 const Register = lazy(() => import('./auth/register'))
 const Dashboard = lazy(() => import('./dashboard/index'))
+const Employee = lazy(() => import('./dashboard/employee/index'))
+const Criteria = lazy(() => import('./dashboard/criteria/index'))
+const Setting = lazy(() => import('./dashboard/settings/index'))
+const Account = lazy(() => import('./dashboard/settings/account/index'))
 
 const useRoutes = () => [
   {
-    path: '/',
+    path: PATH.LOGIN || '/',
     component: <Login />,
   },
   {
-    path: '/login',
-    component: <Login />,
-  },
-  {
-    path: '/register',
+    path: PATH.REGISTER,
     component: <Register />,
   },
   {
-    path: '/dashboard',
+    path: PATH.DASHBOARD,
     component: <Dashboard />,
+  },
+  {
+    path: PATH.DASHBOARD_EMPLOYEE,
+    component: <Employee />,
+  },
+  {
+    path: PATH.DASHBOARD_CRITERIA,
+    component: <Criteria />,
+  },
+  {
+    path: PATH.DASHBOARD_ADMIN_SETTINGS,
+    component: <Setting />,
+  },
+  {
+    path: PATH.DASHBOARD_ADMIN_ACCOUNT,
+    component: <Account />,
   },
 ]
 
