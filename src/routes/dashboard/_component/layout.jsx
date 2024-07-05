2f13a8f49e5949a6d32c6da7ec373bdca4979d39
mmulyana <mulyan.t20@gmail.com>
2024-07-06 01:06:02 +0700

    feat: add navbar in layout

--- src/routes/dashboard/_component/layout.jsx
+++ src/routes/dashboard/_component/layout.jsx
@@ -1,10 +1,12 @@
+import Navbar from '../../../components/navbar'
 import Sidebar from '../../../components/sidebar'
 
 export default function DashboardLayout({ children }) {
   return (
     <>
+      <Navbar />
       <Sidebar />
-      <div className='ml-[280px] p-4'>{children}</div>
+      <div className='ml-[280px] p-4 pt-14'>{children}</div>
     </>
   )
 }
