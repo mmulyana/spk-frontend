6ed5f617e68ae3332e902ecdb925f5eb3b07991a
mmulyana <mulyan.t20@gmail.com>
2024-07-06 01:05:33 +0700

    feat: not found page

---
+++ src/routes/not-found/index.jsx
@@ -0,0 +1,11 @@
+import { Center, Flex } from '@mantine/core'
+
+export default function Page() {
+  return (
+    <Flex h='100vh' align='center' direction='column' justify='center'>
+      <Center>
+        <p className='text-3xl text-gray-500'>404</p>
+      </Center>
+    </Flex>
+  )
+}
