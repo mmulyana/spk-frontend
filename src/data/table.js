76fe90120574c700884d6f9a2b380fceb81a9dc4
mmulyana <mulyan.t20@gmail.com>
2024-07-06 06:06:27 +0700

    chore: add data table

---
+++ src/data/table.js
@@ -0,0 +1,45 @@
+export const criteria = [
+  {
+    id: 1,
+    name: 'Sangat baik',
+    value: 10,
+  },
+  {
+    id: 2,
+    name: 'Baik',
+    value: 8,
+  },
+  {
+    id: 3,
+    name: 'Cukup',
+    value: 5,
+  },
+  {
+    id: 4,
+    name: 'Buruk',
+    value: 0,
+  },
+]
+
+export const employees = [
+  {
+    id: 'A1',
+    name: 'Bambang',
+    position: 'pegawai',
+  },
+  {
+    id: 'A2',
+    name: 'Fahmi',
+    position: 'pegawai',
+  },
+  {
+    id: 'A3',
+    name: 'Siti',
+    position: 'intern',
+  },
+  {
+    id: 'A4',
+    name: 'Ilham',
+    position: 'pegawai',
+  },
+]
