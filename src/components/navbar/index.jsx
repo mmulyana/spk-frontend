5822da322a4f9774410e22c6da9ab9614d60195f
mmulyana <mulyan.t20@gmail.com>
2024-07-06 03:25:18 +0700

    chore: change border navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 
   return (
     <div className='fixed pl-[280px] h-fit z-10 w-full'>
-      <div className='h-14 w-full bg-white border-b border-gray-200 px-4 flex items-center justify-between'>
+      <div className='h-14 w-full bg-white border-b border-gray-600/10 px-4 flex items-center justify-between'>
         <p className='text-md'>{title ?? ''}</p>
       </div>
     </div>
