75adcc825853f0c3466a4ca6cde2805c86ad5c2f
mmulyana <mulyan.t20@gmail.com>
2024-07-06 06:07:44 +0700

    feat: update notfound

--- src/routes/not-found/index.jsx
+++ src/routes/not-found/index.jsx
@@ -1,10 +1,17 @@
-import { Center, Flex } from '@mantine/core'
+import { Button, Center, Flex } from '@mantine/core'
+import { Link } from 'react-router-dom'
+import { PATH } from '../../utils/constant/_path'
 
 export default function Page() {
   return (
     <Flex h='100vh' align='center' direction='column' justify='center'>
       <Center>
-        <p className='text-3xl text-gray-500'>404</p>
+        <Flex direction='column' justify='center' align='center' gap={10}>
+          <p className='text-5xl text-gray-800'>404</p>
+          <Link to={PATH.LOGIN}>
+            <Button>Kembali</Button>
+          </Link>
+        </Flex>
       </Center>
     </Flex>
   )
