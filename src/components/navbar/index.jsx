2dde121b5ca84b95208ba2f62fecbafa53eb3543
mmulyana <mulyan.t20@gmail.com>
2024-07-06 01:05:48 +0700

    feat: add navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,14 @@
+import { useAtomValue } from 'jotai'
+import { headerTitle } from '../../atom/common'
+
+export default function Navbar() {
+  const title = useAtomValue(headerTitle)
+
+  return (
+    <div className='fixed pl-[280px] h-fit z-10 w-full'>
+      <div className='h-14 w-full bg-white border-b border-gray-200 px-4 flex items-center justify-between'>
+        <p className='text-md'>{title ?? ''}</p>
+      </div>
+    </div>
+  )
+}
