3a6c9599a09dffde74f912f7bc8edd7a0b13e336
mmulyana <mulyan.t20@gmail.com>
2024-07-06 03:25:41 +0700

    feat: remove module and secondary menu

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,20 +1,14 @@
-import { Text, Group } from '@mantine/core'
+import { Text, Group, Button, Flex } from '@mantine/core'
 
-import classes from './NavbarSearch.module.css'
-import { UserButton } from '../UserButtton'
 import { Link, matchPath } from 'react-router-dom'
 import { PATH } from '../../utils/constant/_path'
+import { ArrowRightStartOnRectangleIcon } from '@heroicons/react/24/solid'
 
 const MainMenu = [
   { label: 'Dashboard', path: PATH.DASHBOARD },
-  { label: 'Pegawai', path: PATH.DASHBOARD_EMPLOYEE },
   { label: 'Kriteria', path: PATH.DASHBOARD_CRITERIA },
+  { label: 'Pegawai', path: PATH.DASHBOARD_EMPLOYEE },
   { label: 'Hasil', path: PATH.DASHBOARD_RESULT },
-]
-
-const SecondaryMenu = [
-  { label: 'Pengaturan', path: PATH.DASHBOARD_ADMIN_SETTINGS },
-  { label: 'Akun', path: PATH.DASHBOARD_ADMIN_ACCOUNT },
 ]
 
 export default function Sidebar() {
@@ -26,10 +20,10 @@ export default function Sidebar() {
         to={menu.path}
         key={menu.label}
         className={[
-          'hover:bg-white border-b-2 hover:border-gray-200 text-sm py-2.5 px-3 rounded-lg',
+          'hover:bg-white text-sm py-2.5 px-3 rounded-lg',
           isActive(menu.path)
-            ? 'bg-white border-gray-200 text-gray-600 font-medium'
-            : 'text-gray-400 border-transparent hover:text-gray-600',
+            ? 'bg-[#F6F7F9] text-gray-600 font-medium hover:bg-[#EBEDF1]'
+            : 'text-gray-400 hover:text-gray-600 hover:bg-[#F6F7F9]',
         ].join(' ')}
       >
         {menu.label}
@@ -37,12 +31,11 @@ export default function Sidebar() {
     ))
 
   return (
-    <nav className={`${classes.navbar} !p-4`}>
-      <div className='w-full'>
-        <UserButton />
+    <nav className='px-4 border-r border-gray-600/10 w-[280px] fixed h-screen bg-white'>
+      <div className='w-full h-14 px-4 relative'>
+        <div className='w-full h-[1.5px] bg-gray-100 absolute bottom-0 left-1/2 -translate-x-1/2'></div>
       </div>
-
-      <div>
+      <div className='mt-4'>
         <Group justify='space-between' className='mb-2'>
           <Text size='xs' fw={500} c='dimmed'>
             Menus
@@ -51,13 +44,18 @@ export default function Sidebar() {
         <div className='flex flex-col gap-2'>{renderMenu(MainMenu)}</div>
       </div>
 
-      <div className='mt-4'>
-        <Group justify='space-between' className='mb-2'>
-          <Text size='xs' fw={500} c='dimmed'>
-            Pengaturan
-          </Text>
-        </Group>
-        <div className='flex flex-col gap-2'>{renderMenu(SecondaryMenu)}</div>
+      <div className='px-4 absolute bottom-4 left-1/2 -translate-x-1/2 h-fit w-full'>
+        <Button
+          w='100%'
+          unstyled
+          className='border border-gray-600/20 text-gray-500 rounded-md text-sm'
+          py={6}
+        >
+          <Flex align='center' justify='center' gap={4}>
+            <ArrowRightStartOnRectangleIcon className='h-5 w-5 text-gray-400' />
+            <span className='font-normal'>Logout</span>
+          </Flex>
+        </Button>
       </div>
     </nav>
   )
