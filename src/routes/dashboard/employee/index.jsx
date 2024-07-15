980f5fb55a5856d4b1289838b23cd57485ad1a74
mmulyana <mulyan.t20@gmail.com>
2024-07-16 01:52:01 +0700

    feat: new modal employee

--- src/routes/dashboard/employee/index.jsx
+++ src/routes/dashboard/employee/index.jsx
@@ -7,6 +7,7 @@ import {
   Pagination,
   Select,
   Table,
+  Textarea,
   TextInput,
 } from '@mantine/core'
 import { useMemo, useState } from 'react'
@@ -50,17 +51,61 @@ export default function Page() {
     []
   )
 
+  const onDeleteModal = (onClose) => {
+    onClose?.()
+    setTimeout(() => setModalState('add'), 500)
+  }
+
   const modalOpenConfig = {
     add: {
       component: (
-        <form>
-          <TextInput label='Nama' />
-          <Select
-            mt={8}
-            label='Jabatan'
-            placeholder='select'
-            data={['Pegawai', 'Intern', 'Supervisor']}
-          />
+        <form className='w-full'>
+          <Flex gap={16} w='100%'>
+            <TextInput w='50%' label='Nama' />
+            <TextInput w='50%' label='NIP' />
+          </Flex>
+          <Flex gap={16} w='100%' mt={16}>
+            <TextInput w='50%' label='Tempat Lahir' />
+            <TextInput w='50%' label='Tanggal Lahir' type='date' />
+          </Flex>
+          <Flex gap={16} w='100%' mt={16}>
+            <Select
+              w='50%'
+              label='Jenis Kelamin'
+              placeholder='Pilih Jenis Kelamin'
+              data={['Laki-laki', 'perempuan']}
+            />
+            <TextInput w='50%' label='Agama' />
+          </Flex>
+          <Textarea mt={16} label='Alamat' />
+          <Flex gap={16} w='100%' mt={16}>
+            <Select
+              w='50%'
+              label='Pendidikan Terakhir'
+              placeholder='Pilih Pendidikan Terakhir'
+              data={['SMP', 'SMA', 'D1', 'D2', 'D3', 'D4', 'S1', 'S2', 'S3']}
+            />
+            <Select
+              w='50%'
+              label='Status Pegawai'
+              placeholder='Pilih Status Pegawai'
+              data={['Tetap', 'Kontrak']}
+            />
+          </Flex>
+          <Flex gap={16} w='100%' mt={16}>
+            <Select
+              w='50%'
+              label='Departemen'
+              placeholder='Pilih Departemen'
+              data={['Produksi', 'Keuangan', 'Marketing', 'Sales']}
+            />
+            <Select
+              w='50%'
+              label='Jabatan'
+              placeholder='Pilih Jabatan'
+              data={['Intern', 'Pegawai', 'Supervisor']}
+            />
+          </Flex>
           <Button
             mt={20}
             display='block'
@@ -73,40 +118,46 @@ export default function Page() {
         </form>
       ),
       title: 'Tambah Pegawai',
+      size: 'xl',
     },
     afterAdd: {
       component: (
         <div>
           <p className='text-lg text-center'>Buat Penilaian Pegawai Baru</p>
-          <div className='flex gap-2 items-center mt-2 justify-center'>
+          <Flex gap={16} w='100%' mt={24}>
             <Button
-              display='block'
+              w='50%'
               size='sm'
               variant='default'
               onClick={() => setModalState('add')}
             >
               Tambah Pegawai Baru
             </Button>
-            <Button
-              display='block'
-              size='sm'
-              onClick={() => setModalState('mark')}
-            >
+            <Button w='50%' size='sm' onClick={() => setModalState('mark')}>
               Buat Penilaian
             </Button>
-          </div>
+          </Flex>
         </div>
       ),
-      title: 'Pegawai Berhasil Ditambah',
+      title: '',
+      size: 'sm',
     },
     mark: {
       component: (
         <div>
           <p>Penilaian Pegawai</p>
-          <Button onClick={closeAdd}>Selesai</Button>
+          <Button
+            onClick={() => {
+              closeAdd()
+              setModalState('add')
+            }}
+          >
+            Selesai
+          </Button>
         </div>
       ),
       title: 'Penilaian Pegawai',
+      size: 'xl',
     },
   }
 
@@ -193,17 +244,62 @@ export default function Page() {
         </div>
       </DashboardLayout>
 
-      <Modal opened={openedEdit} onClose={handleClose} title='Edit Pegawai'>
-        <form>
-          <TextInput label='Nama' />
-          <Select
-            mt={8}
-            label='Jabatan'
-            placeholder='select'
-            data={['Pegawai', 'Intern', 'Supervisor']}
-          />
-          <Button mt={20} display='block' size='sm' ml='auto'>
-            Simpan
+      <Modal opened={openedEdit} onClose={handleClose} title='Edit Pegawai' size='xl'>
+        <form className='w-full'>
+          <Flex gap={16} w='100%'>
+            <TextInput w='50%' label='Nama' />
+            <TextInput w='50%' label='NIP' />
+          </Flex>
+          <Flex gap={16} w='100%' mt={16}>
+            <TextInput w='50%' label='Tempat Lahir' />
+            <TextInput w='50%' label='Tanggal Lahir' type='date' />
+          </Flex>
+          <Flex gap={16} w='100%' mt={16}>
+            <Select
+              w='50%'
+              label='Jenis Kelamin'
+              placeholder='Pilih Jenis Kelamin'
+              data={['Laki-laki', 'perempuan']}
+            />
+            <TextInput w='50%' label='Agama' />
+          </Flex>
+          <Textarea mt={16} label='Alamat' />
+          <Flex gap={16} w='100%' mt={16}>
+            <Select
+              w='50%'
+              label='Pendidikan Terakhir'
+              placeholder='Pilih Pendidikan Terakhir'
+              data={['SMP', 'SMA', 'D1', 'D2', 'D3', 'D4', 'S1', 'S2', 'S3']}
+            />
+            <Select
+              w='50%'
+              label='Status Pegawai'
+              placeholder='Pilih Status Pegawai'
+              data={['Tetap', 'Kontrak']}
+            />
+          </Flex>
+          <Flex gap={16} w='100%' mt={16}>
+            <Select
+              w='50%'
+              label='Departemen'
+              placeholder='Pilih Departemen'
+              data={['Produksi', 'Keuangan', 'Marketing', 'Sales']}
+            />
+            <Select
+              w='50%'
+              label='Jabatan'
+              placeholder='Pilih Jabatan'
+              data={['Intern', 'Pegawai', 'Supervisor']}
+            />
+          </Flex>
+          <Button
+            mt={20}
+            display='block'
+            size='sm'
+            ml='auto'
+            onClick={() => setModalState('afterAdd')}
+          >
+            Tambah
           </Button>
         </form>
       </Modal>
@@ -221,8 +317,9 @@ export default function Page() {
 
       <Modal
         opened={openedAdd}
-        onClose={handleClose}
+        onClose={() => onDeleteModal(closeAdd)}
         title={modalOpenConfig[modalState].title}
+        size={modalOpenConfig[modalState].size}
       >
         {modalOpenConfig[modalState].component}
       </Modal>
