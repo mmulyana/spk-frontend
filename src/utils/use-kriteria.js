4e22d5a9656c6a4b21954cb47711f414abd4894b
mmulyana <mulyan.t20@gmail.com>
2024-07-24 22:52:07 +0700

    feat: use-kriteria

---
+++ src/utils/use-kriteria.js
@@ -0,0 +1,86 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { URL } from './constant/_urls'
+import http from './http'
+import { KEYS } from './constant/_key'
+import { toast } from 'sonner'
+
+const createKriteria = async (payload) => {
+  return await http.post(URL.KRITERIA, payload)
+}
+const updateKriteria = async (payload) => {
+  return await http.patch(`${URL.KRITERIA}/${payload.id}`, payload)
+}
+const deleteKriteria = async (id) => {
+  return await http.delete(`${URL.KRITERIA}/${id}`)
+}
+const getKriteria = async (id) => {
+  if (!!id) return await http(`${URL.KRITERIA}/${id}`)
+  return await http(URL.KRITERIA)
+}
+
+export const useKriteria = () => {
+  return useQuery({
+    queryFn: getKriteria,
+    queryKey: [KEYS.KRITERIA],
+  })
+}
+
+export const useDetailKriteria = (id) => {
+  return useQuery({
+    queryFn: () => getKriteria(id),
+    queryKey: [KEYS.KRITERIA, id],
+    enabled: !!id,
+  })
+}
+
+export const useCreateKriteria = () => {
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: createKriteria,
+    onError: (error) => {
+      toast.error('Gagal menambah kriteria, silahkan ulangi')
+    },
+    onSuccess: (data) => {
+      toast.success('Kriteria berhasil ditambahkan')
+      queryClient.invalidateQueries([KEYS.KRITERIA])
+    },
+  })
+
+  return { mutate }
+}
+
+export const useUpdateKriteria = (id) => {
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: updateKriteria,
+    onError: (error) => {
+      toast.error('Gagal mengupdate kriteria, silahkan ulangi')
+    },
+    onSuccess: (data) => {
+      toast.success('Kriteria berhasil diupdate')
+      queryClient.invalidateQueries([KEYS.KRITERIA])
+    },
+  })
+
+  return { mutate }
+}
+
+export const useDeleteKriteria = (id) => {
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: deleteKriteria,
+    onError: (error) => {
+      toast.error('Gagal menghapus kriteria, silahkan ulangi')
+    },
+    onSuccess: (data) => {
+      toast.success('Kriteria berhasil dihapus')
+      queryClient.invalidateQueries([KEYS.KRITERIA, id])
+      queryClient.invalidateQueries([KEYS.KRITERIA])
+    },
+  })
+
+  return { mutate }
+}
