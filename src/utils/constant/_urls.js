93b8c6fadebcebc7b38985a31b1b54256629a61b
mmulyana <mulyan.t20@gmail.com>
2024-07-24 22:29:32 +0700

    feat: add urls constant

---
+++ src/utils/constant/_urls.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://localhost:5000/api'
+export const URL = {
+  LOGIN: BASE_URL + '/login',
+  REGISTER: BASE_URL + '/register',
+  PEGAWAI: BASE_URL + '/pegawai',
+  KRITERIA: BASE_URL + '/kriteria',
+  DASHBOARD_WIDGET: BASE_URL + '/dashboard/widget',
+  AKUN: BASE_URL + '/akun',
+}
