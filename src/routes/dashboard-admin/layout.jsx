aafd439e62ac5ae9870163fc60bca4545fc2a906
mmulyana <mulyan.t20@gmail.com>
2024-07-28 05:23:40 +0700

    feat: authorization in layout dashboard

--- src/routes/dashboard-admin/layout.jsx
+++ src/routes/dashboard-admin/layout.jsx
@@ -1,7 +1,37 @@
-import Navbar from '../../components/navbar'
+import { useAtom } from 'jotai'
+import { CookieKeys, CookieStorage } from '../../utils/cookie'
+import { useNavigate } from 'react-router-dom'
 import Sidebar from '../../components/sidebar'
+import Navbar from '../../components/navbar'
+import { userAtom } from '../../atom/user'
+import { jwtDecode } from 'jwt-decode'
+import { useEffect } from 'react'
+import { PATH } from '../../utils/constant/_path'
 
 export default function DashboardAdminLayout({ children }) {
+  const [user, setUser] = useAtom(userAtom)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user !== null) return
+
+    const token = CookieStorage.get(CookieKeys.AuthToken)
+
+    if (!token) {
+      navigate(PATH.LOGIN)
+      return
+    }
+
+    const decoded = jwtDecode(token)
+
+    if (decoded.role !== 'ADMIN') {
+      navigate(PATH.DASHBOARD)
+      return
+    }
+
+    setUser({ nama: decoded.nama, email: decoded.email, role: decoded.role })
+  }, [user])
+
   return (
     <>
       <Navbar />
