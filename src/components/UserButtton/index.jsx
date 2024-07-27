f01cb0be74fe682fe2541a91a4d8ca286447347d
mmulyana <mulyan.t20@gmail.com>
2024-07-27 18:41:21 +0700

    feat: add user atom in userButton

--- src/components/UserButtton/index.jsx
+++ src/components/UserButtton/index.jsx
@@ -1,20 +1,21 @@
 import { UnstyledButton, Group, Avatar, Text, rem, Flex } from '@mantine/core'
+import { useAtomValue } from 'jotai'
+import { userAtom } from '../../atom/user'
 
 export function UserButton() {
+  const user = useAtomValue(userAtom)
+
   return (
     <div>
       <Flex gap={8}>
-        <Avatar
-          src='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png'
-          radius='xl'
-        />
+        <Avatar radius='xl' className='uppercase'>{user?.nama?.at(0)}</Avatar>
         <div>
           <Text size='sm' fw={500}>
-            Example
+            {user?.nama || ''}
           </Text>
 
           <Text c='dimmed' size='xs'>
-            example@mail.com
+            {user?.email || ''}
           </Text>
         </div>
       </Flex>
