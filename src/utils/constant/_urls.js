4d59a60e034152a6bbcd731464bdd574a08fe4c7
mmulyana <mulyan.t20@gmail.com>
2024-07-28 09:43:56 +0700

    fix: url

--- src/utils/constant/_urls.js
+++ src/utils/constant/_urls.js
@@ -1,9 +1,11 @@
 const BASE_URL = 'http://localhost:5000/api'
+
 export const URL = {
   LOGIN: BASE_URL + '/login',
   REGISTER: BASE_URL + '/register',
   PEGAWAI: BASE_URL + '/pegawai',
   KRITERIA: BASE_URL + '/kriteria',
-  DASHBOARD_WIDGET: BASE_URL + '/dashboard/widget',
+  DASHBOARD: BASE_URL + '/dashboard',
   AKUN: BASE_URL + '/akun',
+  SPK: BASE_URL + '/spk',
 }
