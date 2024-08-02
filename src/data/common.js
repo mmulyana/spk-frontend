d9ee9ab8816bb39a024fe45e17db4c94c608d794
mmulyana <mulyan.t20@gmail.com>
2024-08-02 22:03:57 +0700

    feat: add common data

---
+++ src/data/common.js
@@ -0,0 +1,14 @@
+export const role = ['Intern', 'Pegawai', 'Supervisor', 'Lead']
+export const education = [
+  'SMP',
+  'SMA',
+  'D1',
+  'D2',
+  'D3',
+  'D4',
+  'S1',
+  'S2',
+  'S3',
+].reverse()
+export const departement = ['Produksi', 'Keuangan', 'Marketing', 'Sales']
+export const status = ['Tetap', 'Kontrak']
