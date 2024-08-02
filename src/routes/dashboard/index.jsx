41264348b63db7d5825c7054e6dc3909fcadf098
mmulyana <mulyan.t20@gmail.com>
2024-08-03 01:27:04 +0700

    feat: update dashboard

--- src/routes/dashboard/index.jsx
+++ src/routes/dashboard/index.jsx
@@ -10,6 +10,7 @@ import {
   UsersIcon,
 } from '@heroicons/react/24/solid'
 import { useDashboard } from '../../utils/use-dashboard'
+import { roundUpToDecimalPlace } from '../../utils/decimal'
 
 export default function MainDashboard() {
   useTitle('Dashboard')
@@ -29,9 +30,10 @@ export default function MainDashboard() {
 
   const employeeRows = data?.pegawai?.map((d, index) => (
     <Table.Tr key={index} className='hover:bg-gray-50/50'>
-      <Table.Td>{d.id}</Table.Td>
-      <Table.Td>{d.nama}</Table.Td>
-      <Table.Td>{d.jabatan}</Table.Td>
+      <Table.Td>{d.pegawai.nama}</Table.Td>
+      <Table.Td>{d.pegawai.NIP}</Table.Td>
+      <Table.Td>{d.pegawai.jabatan}</Table.Td>
+      <Table.Td>{roundUpToDecimalPlace(d.nilai, 1)}</Table.Td>
     </Table.Tr>
   ))
 
@@ -39,7 +41,7 @@ export default function MainDashboard() {
     <DashboardLayout>
       <div className='mt-4'>
         <div className='grid grid-cols-4 gap-4'>
-          <div className='bg-white rounded-lg border border-gray-600/10 h-fit p-4'>
+          <div className='bg-white rounded-lg border border-gray-600/10 h-22 p-4'>
             <Flex align='center' gap={8}>
               <div className='h-10 w-10 rounded-full bg-blue-100 flex items-center justify-center m-0'>
                 <UsersIcon className='h-5 w-5 text-blue-600' />
@@ -52,20 +54,20 @@ export default function MainDashboard() {
               </div>
             </Flex>
           </div>
-          <div className='bg-white rounded-lg border border-gray-600/10 h-fit p-4'>
+          <div className='bg-white rounded-lg border border-gray-600/10 h-22 p-4'>
             <Flex align='center' gap={8}>
-              <div className='h-10 w-10 rounded-full bg-blue-100 flex items-center justify-center m-0'>
+              <div className='flex-0 h-10 w-10 rounded-full bg-blue-100 flex items-center justify-center m-0'>
                 <TrophyIcon className='h-5 w-5 text-blue-600' />
               </div>
-              <div>
-                <p className='text-sm text-gray-400'>Pegawai Terbaik</p>
-                <p className='text-4xl font-medium text-gray-700'>
-                  {data?.count?.sudah}
+              <div className='flex-1'>
+                <p className='text-sm text-gray-400'>Pegawai Dinilaii</p>
+                <p className='text-2xl font-medium text-gray-700'>
+                  {data?.count?.nilai} <span className='text-sm text-gray-400 font-normal'>dari</span>  {data?.count?.pegawai}
                 </p>
               </div>
             </Flex>
           </div>
-          <div className='bg-white rounded-lg border border-gray-600/10 h-fit p-4'>
+          <div className='bg-white rounded-lg border border-gray-600/10 h-22 p-4'>
             <Flex align='center' gap={8}>
               <div className='h-10 w-10 rounded-full bg-blue-100 flex items-center justify-center m-0'>
                 <FunnelIcon className='h-5 w-5 text-blue-600' />
@@ -78,7 +80,7 @@ export default function MainDashboard() {
               </div>
             </Flex>
           </div>
-          <div className='bg-white rounded-lg border border-gray-600/10 h-fit p-4'>
+          <div className='bg-white rounded-lg border border-gray-600/10 h-22 p-4'>
             <Flex align='center' gap={8}>
               <div className='h-10 w-10 rounded-full bg-blue-100 flex items-center justify-center m-0'>
                 <UserIcon className='h-5 w-5 text-blue-600' />
@@ -92,7 +94,7 @@ export default function MainDashboard() {
             </Flex>
           </div>
         </div>
-        <div className='mt-4 grid grid-cols-2 gap-4'>
+        <div className='grid grid-cols-2 gap-4'>
           <div className='bg-white p-4 mt-4 rounded-lg border border-gray-600/10'>
             <div className='flex items-center justify-between mb-4'>
               <p className='text-gray-700 font-medium'>Pegawai Terbaik</p>
@@ -100,16 +102,18 @@ export default function MainDashboard() {
             <Table className='rounded-md'>
               <Table.Thead>
                 <Table.Tr className='!border-none'>
-                  <Table.Th className='rounded-l-md bg-[#F6F7F9]'>
-                    <span className='text-sm font-medium'>Id</span>
-                  </Table.Th>
                   <Table.Th className='bg-[#F6F7F9]'>
                     <span className='text-sm font-medium'>Nama</span>
                   </Table.Th>
                   <Table.Th className='bg-[#F6F7F9]'>
+                    <span className='text-sm font-medium'>NIP</span>
+                  </Table.Th>
+                  <Table.Th className='bg-[#F6F7F9]'>
                     <span className='text-sm font-medium'>Jabatan</span>
                   </Table.Th>
-                  <Table.Th className='w-[120px] bg-[#F6F7F9] rounded-r-md'></Table.Th>
+                  <Table.Th className='bg-[#F6F7F9]'>
+                    <span className='text-sm font-medium'>Nilai</span>
+                  </Table.Th>
                 </Table.Tr>
               </Table.Thead>
               <Table.Tbody>{employeeRows}</Table.Tbody>
